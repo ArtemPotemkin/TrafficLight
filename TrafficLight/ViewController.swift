99fda0d66f8d15b3c6c0e6eb8098bb92782d5354
Artem Potemkin <art.potemkin@gmail.com>
2023-05-27 14:40:38 +0300

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by Артём Потёмкин on 27.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
