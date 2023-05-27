fcaa5b0c466f8613421f9d95167588afab7520ed
Artem Potemkin <art.potemkin@gmail.com>
2023-05-27 15:22:50 +0300

    Setup Logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private let isOn: CGFloat = 1
+    private let isOff: CGFloat = 0.3
+    private var currentLight: CurrentLight = .red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = redLight.frame.width / 2
+        greenLight.layer.cornerRadius = redLight.frame.width / 2
+        
     }
 
-
+    @IBAction func startButtotPressed() {
+        if startButton.titleLabel?.text == "Start" {
+            startButton.setTitle("Next", for: .normal)
+        }
+        switch currentLight {
+            
+        case .red:
+            redLight.alpha = isOn
+            greenLight.alpha = isOff
+            yellowLight.alpha = isOff
+            currentLight = .yellow
+        case .yellow:
+            yellowLight.alpha = isOn
+            redLight.alpha = isOff
+            greenLight.alpha = isOff
+            currentLight = .green
+        case .green:
+            greenLight.alpha = isOn
+            yellowLight.alpha = isOff
+            redLight.alpha = isOff
+            currentLight = .red
+        }
+    }
+    
 }
 
